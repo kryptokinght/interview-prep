ef073049324e5e70d9bf5cc4c66788367f69d792
Minanshu Singh <minanshu.1998@gmail.com>
2019-01-15 17:10:54 +0530

    add starting points for different platforms

---
+++ codezen.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+  int n = 10;
+  long long *r = new long long[n];
+  long long *l = new long long[n];
+  r[9] = 100;
+  cout << r[9] << endl;
+  return 0;
+}
--- conversions.cpp
+++ conversions.cpp
@@ -3,5 +3,8 @@ using namespace std;
 
 int main()
 {
+  int x = 999923492349234;
+  // integer to string
+
   return 0;
 }
